ed08f800c8701a11451b48eac8ac0deeac85192c
Kadir Hali <kadir.hali@iksirsoft.com>
2023-07-31 13:35:22 +0300

    Add project files.

---
+++ HTMLAgilityPack/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace HTMLAgilityPack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Uri url = new Uri("http://www.onedio.com");
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
+            dokuman.LoadHtml(html);
+            HtmlNodeCollection basliklar = dokuman.DocumentNode.SelectNodes("//a");
+            foreach (var baslik in basliklar)
+            {
+                string link = baslik.Attributes["href"].Value;
+                listBox1.Items.Add(baslik.InnerText);
+            }
+        }
+    }
+}
