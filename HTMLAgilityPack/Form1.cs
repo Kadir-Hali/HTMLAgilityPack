50f609a1e9bed67451f92b610a989017d3c96818
Kadir Hali <kadir.hali@iksirsoft.com>
2023-08-08 17:54:44 +0300

    Açıklama eklendi

--- HTMLAgilityPack/Form1.cs
+++ HTMLAgilityPack/Form1.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Net;
-using System.Security.Policy;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using HtmlAgilityPack;
 
 namespace HTMLAgilityPack
 {
@@ -22,17 +15,16 @@ namespace HTMLAgilityPack
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Uri url = new Uri("http://www.onedio.com");
+            Uri url = new Uri("https://www.trendyol.com/arzum/ok004-k-okka-minio-turk-kahvesi-makinesi-krom-p-415310?boutiqueId=620857&merchantId=105330");
             WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
             string html = client.DownloadString(url);
             HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
             dokuman.LoadHtml(html);
-            HtmlNodeCollection basliklar = dokuman.DocumentNode.SelectNodes("//a");
-            foreach (var baslik in basliklar)
-            {
-                string link = baslik.Attributes["href"].Value;
-                listBox1.Items.Add(baslik.InnerText);
-            }
+            var baslik = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/h1/span").First().InnerText;
+            var aciklama = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/section/div/div/div").First().InnerText;
+            listBox1.Items.Add("Ürün Adı : " + baslik);
+            listBox1.Items.Add("Ürün Açıklaması : " + aciklama);
         }
     }
 }
