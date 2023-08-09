1bb2211745ca32c20d1f5aea7812a5ce6341c946
Kadir Hali <kadir.hali@iksirsoft.com>
2023-08-09 23:40:47 +0300

    -info added

--- HTMLAgilityPack/Form1.Designer.cs
+++ HTMLAgilityPack/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 349);
+            this.button1.Location = new System.Drawing.Point(39, 566);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(109, 45);
             this.button1.TabIndex = 0;
@@ -48,18 +50,28 @@
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(39, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(732, 324);
+            this.listBox1.Size = new System.Drawing.Size(1412, 324);
             this.listBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 364);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1056, 179);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1463, 623);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- HTMLAgilityPack/Form1.cs
+++ HTMLAgilityPack/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,18 +14,29 @@ namespace HTMLAgilityPack
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//TRENDYOL
         {
-            Uri url = new Uri("https://www.trendyol.com/arzum/ok004-k-okka-minio-turk-kahvesi-makinesi-krom-p-415310?boutiqueId=620857&merchantId=105330");
+            //Uri url = new Uri("https://www.trendyol.com/arzum/ok004-k-okka-minio-turk-kahvesi-makinesi-krom-p-415310?boutiqueId=620857&merchantId=105330");
+            Uri url = new Uri("https://www.trendyol.com/reeder/p13-blue-max-2022-4-gb-128-gb-yesil-cep-telefonu-reeder-turkiye-garantili-p-299146129?boutiqueId=61&merchantId=364277");
             WebClient client = new WebClient();
             client.Encoding = Encoding.UTF8;
             string html = client.DownloadString(url);
             HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
             dokuman.LoadHtml(html);
             var baslik = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/h1/span").First().InnerText;
-            var aciklama = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/section/div/div/div").First().InnerText;
+            var marka = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/h1/a").First().InnerText;
+            var bilgi = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/section/div/div/div").First().InnerText;
+            var urunOzellikleri = dokuman.DocumentNode.SelectNodes("//*[@id=\"product-detail-app\"]/div/section/div/ul").First().InnerText;
+            //var urunAciklaması = dokuman.DocumentNode.SelectNodes("//*[@id=\"urun-aciklamasi\"]/div").First().InnerText;
+            var picLink = dokuman.DocumentNode.SelectSingleNode("//*[@id=\"product-detail-app\"]/div/div[2]/div[1]/div[2]/div[1]/div/div[1]/div/div/img").Attributes["src"].Value;
+            //var imageStream = HttpWebRequest.Create(imagePath).GetResponse().GetResponseStream();
+            //this.pictureBox1.Image=Image.FromStream(imageStream);
             listBox1.Items.Add("Ürün Adı : " + baslik);
-            listBox1.Items.Add("Ürün Açıklaması : " + aciklama);
+            listBox1.Items.Add("Marka : " + marka);
+            listBox1.Items.Add("Ürün Bilgileri : " + bilgi);
+            listBox1.Items.Add("Ürün Özellikleri : " + urunOzellikleri);
+            //listBox1.Items.Add("Ürün Açıklaması : " + urunAciklaması); TÜM ÜRÜNLERDE BU ALAN YOK!
+            listBox1.Items.Add("Ürün Foto Linki : " + picLink);
         }
     }
 }
